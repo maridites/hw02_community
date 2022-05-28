84d0eb4da1d8177f4b20183262a8a95f018cba33
Мария Артемова <masha2017.z@gmail.com>
2022-05-28 18:45:04 +0300

    3 version

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,6 +8,7 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return (self.title[:20])
@@ -18,16 +19,18 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
+        on_delete=models.CASCADE,
         related_name='posts'
     )
     group = models.ForeignKey(
-        Group, 
-        on_delete=models.SET_NULL, 
+        Group,
+        on_delete=models.SET_NULL,
         blank=True,
-        null=True, 
+        null=True,
         verbose_name='Группа поста',
         related_name='posts'
     )
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return (self.text[:30])
